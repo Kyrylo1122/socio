330a8d877b6139c4b3a15e5325d8ec986c31b55d
Kyrylo <bereznov.kyrylo@mail.ru>
2024-01-27 17:30:53 +0100

    add netlify.toml

--- src/utils/dataURLtoFile.tsx
+++ src/utils/dataURLtoFile.tsx
@@ -1,7 +1,7 @@
 const dataURLtoFile = (dataurl: string, filename: string) => {
   const arr = dataurl.split(",");
 
-  const mime = arr[0].match(/:(.*?);/)[1];
+  const mime = arr[0].match(/:(.*?);/)![1];
   const bstr = atob(arr[1]);
   let n = bstr.length;
   const u8arr = new Uint8Array(n);
